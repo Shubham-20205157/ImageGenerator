846676c3a5ef3acc6eb54eef8672e7c4cb8dad8a
Shubahm <shubham.20205157@mnnit.ac.in>
2023-07-06 22:33:01 +0530

    frontend Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <p class="text-lg font-medium">
+    “Tailwind CSS is the only framework that I've seen scale
+    on large teams. It’s easy to customize, adapts to any design,
+    and the build size is tiny.”
+    </p>
+  )
+}
+export default App;
